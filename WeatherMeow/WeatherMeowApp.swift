7e4bdb94df4156ff763122b3e3a2634de6d4a28e
영현 <yh97yhyh@naver.com>
2024-02-28 18:12:16 +0900

    first commit

---
+++ WeatherMeow/WeatherMeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMeowApp.swift
+//  WeatherMeow
+//
+//  Created by 영현 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
